54774b6e3223150cf4f5e867919abadbddb00a58
sugyanmishra29 <sugyanmishra.sinu@gmail.com>
2019-10-27 17:09:31 +0530

    sign in and sign up done

--- app/src/main/java/com/example/foodcourt/signinpage.java
+++ app/src/main/java/com/example/foodcourt/signinpage.java
@@ -1,12 +1,22 @@
 package com.example.foodcourt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class signinpage extends AppCompatActivity {
 
@@ -14,6 +24,40 @@ public class signinpage extends AppCompatActivity {
     private EditText siPassword;
     private Button btnSignIn;
     private Button btnResetSi;
+
+    private void checkLogin(){
+        final String email_txt = siUsername.getText().toString().trim();
+        final String password_txt = siPassword.getText().toString().trim();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        ref.child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    if (dataSnapshot.child(email_txt).exists()){
+                        if (dataSnapshot.child(email_txt).getValue().toString().equals(password_txt)){
+                            Intent i = new Intent(signinpage.this,MainActivity.class);
+                            startActivity(i);
+                        }
+
+                        else{
+                            Toast.makeText(signinpage.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    else{
+                        Toast.makeText(signinpage.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(signinpage.this, "Error. Try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +81,7 @@ public class signinpage extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                checkLogin();
             }
         });
 
--- app/src/main/java/com/example/foodcourt/signuppage.java
+++ app/src/main/java/com/example/foodcourt/signuppage.java
@@ -1,20 +1,37 @@
 package com.example.foodcourt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class signuppage extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class signuppage extends AppCompatActivity {
 
     private EditText suUsername;
     private EditText suPassword;
     private Button btnSignUp;
     private Button btnResetSu;
+
+    private void addData()
+    {
+        final String email_txt = suUsername.getText().toString().trim();
+        final String password_txt = suPassword.getText().toString().trim();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("Users").child(email_txt).setValue(password_txt);
+
+        Intent i = new Intent(signuppage.this,signinpage.class);
+        startActivity(i);
+        Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,9 +55,10 @@ public class signuppage extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addData();
             }
         });
+
 
 
 
