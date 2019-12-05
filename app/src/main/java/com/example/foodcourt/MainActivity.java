589dff459c892cfb1242efb65f4fcd44dc62fff4
Madhur Prakash <madhurprakash46@gmail.com>
2019-12-05 23:42:42 +0530

    Shop List Structure Included

--- app/src/main/java/com/example/foodcourt/MainActivity.java
+++ app/src/main/java/com/example/foodcourt/MainActivity.java
@@ -3,23 +3,37 @@ package com.example.foodcourt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mshopnames=new ArrayList<>();
+    private ArrayList<String> mshoplocations=new ArrayList<>();
+    private ArrayList<String> mshoprating=new ArrayList<>();
+
     SharedPreferences sp; //sp-the name of shared preferences has to be the same in both the files
     SharedPreferences.Editor editor;//editor-the name of the editor can be different in both the files
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: Started");
+
         setContentView(R.layout.activity_main);
         sp = getSharedPreferences("login",MODE_PRIVATE);
         editor = sp.edit();
@@ -34,7 +48,56 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finishAffinity();
         }
+        initLoading();
     }
+    //loading for content for recycler view
+    private void initLoading()
+    {
+        mshopnames.add("Shop No. 1");
+        mshopnames.add("Shop No. 2");
+        mshopnames.add("Shop No. 3");
+        mshopnames.add("Shop No. 4");
+        mshopnames.add("Shop No. 5");
+        mshopnames.add("Shop No. 6");
+        mshopnames.add("Shop No. 7");
+        mshopnames.add("Shop No. 8");
+        mshopnames.add("Shop No. 9");
+        mshopnames.add("Shop No. 10");
+
+        mshoplocations.add("Location No. 1");
+        mshoplocations.add("Location No. 2");
+        mshoplocations.add("Location No. 3");
+        mshoplocations.add("Location No. 4");
+        mshoplocations.add("Location No. 5");
+        mshoplocations.add("Location No. 6");
+        mshoplocations.add("Location No. 7");
+        mshoplocations.add("Location No. 8");
+        mshoplocations.add("Location No. 9");
+        mshoplocations.add("Location No. 10");
+
+        mshoprating.add("Stars : 2.5");
+        mshoprating.add("Stars : 4.3");
+        mshoprating.add("Stars : 3.5");
+        mshoprating.add("Stars : 3.3");
+        mshoprating.add("Stars : 4.3");
+        mshoprating.add("Stars : 2.5");
+        mshoprating.add("Stars : 1.6");
+        mshoprating.add("Stars : 3.6");
+        mshoprating.add("Stars : 4.3");
+        mshoprating.add("Stars : 3.9");
+
+        initRecyclerView();
+    }
+    // For Initiating RecyclerView
+
+    private void initRecyclerView()
+    {
+        RecyclerView recyclerView=findViewById(R.id.rv);
+        Recyclerviewadapter recyclerviewadapter=new Recyclerviewadapter(mshopnames,mshoplocations,mshoprating,this);
+        recyclerView.setAdapter(recyclerviewadapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
 
